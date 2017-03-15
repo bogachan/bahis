ba4ec5237a3a807ffd28b880a2bfe317974b50aa
Bogachan Engin <bgchn@yandex.com>
2017-03-15 10:17:45 +0200

    kk

--- app/Http/Controllers/Admin/SliderController.php
+++ app/Http/Controllers/Admin/SliderController.php
@@ -31,11 +31,28 @@ class SliderController extends Controller
         $input = $request->all();
         $bg = $request->file("bg");
         $img = $request->file("img");
-        $input['bg'] = "bg-".$time."-".$bg->getClientOriginalName();
-        $input['img'] = $time."-".$img->getClientOriginalName();
 
 
-      return $input;
+
+        if($resim = $request->file("bg"))
+        {
+            Image::make($resim->getRealPath())->save(public_path("uploads/slider/".$input['bg']));
+            $input['bg'] = "bg-".$time."-".$bg->getClientOriginalName();
+        }
+
+        if($resim2 = $request->file("img"))
+        {
+
+            Image::make($resim2->getRealPath())->save(public_path("uploads/slider/".$input['img']));
+            $input['img'] = $time."-".$img->getClientOriginalName();
+
+        }
+
+
+        Slider::create($input);
+
+        Session::flash('durum',1);
+        return redirect('/admin/slider');
     }
 
     public function show($id)
