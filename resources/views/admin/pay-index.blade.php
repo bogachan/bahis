68d2bc2a3174959c570e43d3e4e770607cd16e04
Bogachan Engin <bgchn@yandex.com>
2017-03-18 18:43:16 +0200

    errre

--- resources/views/admin/pay-index.blade.php
+++ resources/views/admin/pay-index.blade.php
@@ -7,7 +7,10 @@
     </h1>
 @endsection
 
-
+<?php
+$site = \App\Site::all();
+$bank = \App\Bank::all();
+?>
 
 @section('content')
 
@@ -37,13 +40,13 @@
                     <tr>
                         <td>{{$pay->id}}</td>
                         <td>{{ $pay->find($pay->id)->users->name }} / {{ $pay->find($pay->id)->users->username }}</td>
-                        <td>{{DB::table('sites')->where('id',$pay->site_id)->value('name')}}</td>
+                        <td>{{ $site->find($pay->site_id)->name }}</td>
                         <td style="text-align:center">
-                            {{ DB::table('banks')->where('id', $pay->type)->value('name') }}
+                            {{ $bank->find($pay->type)->name }}
                         </td>
                         <td>{{$pay->name}}</td>
                         <td>{{$pay->amount}} TL</td>
-                        <td>{{$pay->bonus}} TL</td>
+                        <td>{{$pay->bonus}} </td>
                         <td>{{$pay->created_at->diffForHumans()}}</td>
                         <td>@if($pay->confirmation == 0) <small class="label bg-green">Kontrol Edilmedi</small>  @elseif($pay->confirmation == 1) <small class="label bg-aqua">Onaylandı</small> @else <small class="label bg-red">İptal Edildi</small>  @endif</td>
                         <td>
@@ -95,7 +98,7 @@
                     <tr>
                         <td>{{$cep->id}}</td>
                         <td>{{ $cep->find($cep->id)->users->name }} / {{ $cep->find($cep->id)->users->username }}</td>
-                        <td>{{DB::table('sites')->where('id',$cep->site_id)->value('name')}}</td>
+                        <td>{{ $site->find($cep->site_id)->name }}</td>
                         <td>{{$cep->cepbank_banka}}</td>
                         <td>{{$cep->amount}} TL</td>
                         <td>{{$cep->name}}</td>
@@ -118,7 +121,6 @@
                 @endforeach
                 </tbody>
             </table>
-
 
         </div>
         <div class="text-center">
