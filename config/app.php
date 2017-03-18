9ac49cf38a7ea44a3b314d48fa8c258620a550d7
Bogachan Engin <bgchn@yandex.com>
2017-03-18 17:23:35 +0200

    mmmksksj

--- config/app.php
+++ config/app.php
@@ -160,7 +160,6 @@ return [
         Collective\Html\HtmlServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
         Cviebrock\EloquentSluggable\ServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
 
 
     ],
@@ -211,7 +210,6 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Image' => Intervention\Image\Facades\Image::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
