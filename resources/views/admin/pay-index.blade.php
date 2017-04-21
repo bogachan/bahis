69b54332014527111e476f8806a2d45773da97f3
Bogachan Engin <bgchn@yandex.com>
2017-04-21 19:00:17 +0300

    mmmnn

--- resources/views/admin/pay-index.blade.php
+++ resources/views/admin/pay-index.blade.php
@@ -87,7 +87,7 @@ $users = \App\User::all();
                     <tr>
                         <td>{{$pay->id}}</td>
                         <td>{{ $pay->find($pay->id)->users->name }} / {{ $pay->find($pay->id)->users->username }}</td>
-                        <td>{{ $site->find($pay->site_id)->name }}</td>
+                        <td>-</td>
                         <td style="text-align:center">
                             {{ $bank->find($pay->type)->name }}
                         </td>
